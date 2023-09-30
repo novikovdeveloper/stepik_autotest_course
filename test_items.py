2e64c76405910b72ca67bf8865f889900afdac78
DeINovikov <deinovikov@beeline.ru>
2023-09-30 17:41:55 +0300

    home_work

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 
 
 class TestBrowserLanguage:
-    def test_find_card_button(self, browser):
+    def test_find_cart_button(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
         browser.get(link)
         add_to_card_button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
