44d59c9cf0ca7ce5b60cb43b11675621cf64f867
DeINovikov <deinovikov@beeline.ru>
2023-09-30 17:51:54 +0300

    home_work

--- test_items.py
+++ test_items.py
@@ -1,10 +1,11 @@
 import time
 from selenium.webdriver.common.by import By
 
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+
 
 class TestBrowserLanguage:
     def test_find_cart_button(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
         browser.get(link)
         add_to_card_button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
         assert add_to_card_button is not None, "button not found on page"
