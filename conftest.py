952a18777de16ae8f0c5f82da5f19f09d8516a08
DeINovikov <deinovikov@beeline.ru>
2023-10-07 22:12:08 +0300

    POM

--- conftest.py
+++ conftest.py
@@ -8,18 +8,18 @@ from selenium.webdriver.firefox.options import Options as OptionsFirefox
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default=None,
                      help="Choose browser: chrome or firefox")
-    parser.addoption('--language', action='store', default=None,
-                     help="Choose language: ec or fr")
+#    parser.addoption('--language', action='store', default=None,
+#                     help="Choose language: ec or fr")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
-    user_language = request.config.getoption("language")
+#    user_language = request.config.getoption("language")
     options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+#    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     options_firefox = OptionsFirefox()
-    options_firefox.set_preference("intl.accept_languages", user_language)
+#    options_firefox.set_preference("intl.accept_languages", user_language)
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
     browser = None
     if browser_name == "chrome":
@@ -30,7 +30,7 @@ def browser(request):
         browser = webdriver.Firefox(options=options_firefox)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
-    browser.implicitly_wait(10)
+    browser.implicitly_wait(20)
     yield browser
     print("\nquit browser..")
     browser.quit()
